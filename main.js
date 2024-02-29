8342dcdf3c892abf4a8055d5df0e26899dfee00b
IgorAzzi <igor.salati@hotmail.com>
2024-02-29 20:06:07 -0300

    Criada a estrutura de repetição While

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ function tocaSomPom () {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;    
 
+let contador = 0;
+   
+
+while (contador < 9) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    
+    contador = contador + 1;
+
+
+}
